0512ec396dd5d1935e4f8fbd9ff5e94beb74c173
Saqib Khan <saqibmk@users.noreply.github.com>
2021-05-19 18:02:30 -0500

    Implemented counting neighbours for cells

--- src/components/Cell/index.js
+++ src/components/Cell/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./cell.css";
 
-const Cell = ({ row, col, value, isClosed }) => {
+const Cell = ({ row, col, value, isClosed, isFlagged }) => {
+  const renderCells = () => {
+    if (!isClosed) {
+      if (value === -1)
+        return (
+          <span role="img" aria-label="bomb">
+            💣
+          </span>
+        );
+      return <div>{value}</div>;
+    }
+  };
   return (
-    <div className={`Cell ${isClosed ? "" : "open"}`}>
-      {!isClosed && <div>{value}</div>}
-    </div>
+    <div className={`Cell ${isClosed ? "" : "open"}`}>{renderCells()}</div>
   );
 };
 
--- src/logic/cells.js
+++ src/logic/cells.js
@@ -1,4 +1,19 @@
 export const cellMaker = () => {
+  const calculateNeighbour = (rowIndex, columnIndex) => {
+    if (cells[rowIndex][columnIndex].value === -1) return -1;
+    let total = 0;
+    for (var xoff = -1; xoff <= 1; xoff++) {
+      for (var yoff = -1; yoff <= 1; yoff++) {
+        const i = xoff + rowIndex;
+        const y = yoff + columnIndex;
+        if (i > -1 && i < 5 && y > -1 && y < 5) {
+          var neighbour = cells[i][y];
+          if (neighbour.value === -1) total++;
+        }
+      }
+    }
+    return total;
+  };
   let cells = [];
 
   for (let i = 0; i < 5; i++) {
@@ -23,5 +38,12 @@ export const cellMaker = () => {
       bombsPlaced++;
     }
   }
+
+  for (let i = 0; i < 5; i++) {
+    for (let x = 0; x < 5; x++) {
+      cells[i][x].value = calculateNeighbour(i, x);
+    }
+  }
+
   return cells;
 };
